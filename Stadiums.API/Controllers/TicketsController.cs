53842e4a890e822cf0efd6c6dacf1b27418c825e
ANDRES FELIPE RAMIREZ USUGA <andresramirez229848@correo.itm.edu.co>
2023-04-28 21:53:02 -0500

    begin controller

---
+++ Stadiums.API/Controllers/TicketsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Stadiums.API.Data;
+using Stadiums.Shared.Entities;
+
+namespace Stadiums.API.Controllers
+{
+    [ApiController]
+    [Route("/api/tickets")]
+
+    public class TicketsController: ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public TicketsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok(await _context.Tickets.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Ticket ticket)
+        {
+            _context.Add(ticket);
+            await _context.SaveChangesAsync();
+            return Ok(ticket);
+        }
+    }
+
+
+
+}
+
